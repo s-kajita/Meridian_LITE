f299813a96a80127c274a92499ae4808658eeee9
Shuuji Kajita <shuuji.kajita@gmail.com>
2025-10-25 10:44:35 +0900

    first commit

--- Meridian_LITE_for_ESP32/src/keys.h
+++ Meridian_LITE_for_ESP32/src/keys.h
@@ -2,11 +2,11 @@
 #define __MERIDIAN_KEYS_H__
 
 // Wifiアクセスポイントの設定
-#define WIFI_AP_SSID  "xxxxxxxx"     // アクセスポイントのWIFI_AP_SSID
-#define WIFI_AP_PASS  "xxxxxxxx"     // アクセスポイントのパスワード
-#define WIFI_SEND_IP  "192.168.1.xx" // 送り先のPCのIPアドレス（PCのIPアドレスを調べておく）
-#define UDP_SEND_PORT 22222          // 送り先のポート番号
-#define UDP_RECV_PORT 22224          // このESP32のポート番号
+#define WIFI_AP_SSID  "Buffalo-G-B278" // アクセスポイントのWIFI_AP_SSID
+#define WIFI_AP_PASS  "kmcteuifcpumh"  // アクセスポイントのパスワード
+#define WIFI_SEND_IP  "192.168.11.3"   // 送り先のPCのIPアドレス（PCのIPアドレスを調べておく）
+#define UDP_SEND_PORT 22222            // 送り先のポート番号
+#define UDP_RECV_PORT 22224            // このESP32のポート番号
 
 // ESP32のIPアドレスを固定する場合は下記の4項目を設定
 #define MODE_FIXED_IP    0                  // IPアドレスを固定するか（0:NO, 1:YES）
